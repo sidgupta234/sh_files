21458a6e89396e34ce2e92f01d4266f0089e78e5
Siddharth Gupta <siddharthgupta234@gmail.com>
2022-07-09 07:40:32 +0530

    add change format

--- preprocess_audio.sh
+++ preprocess_audio.sh
@@ -1,5 +1,10 @@
+cd $1
+mkdir wav
+for k in *; do ffmpeg -i $k wav/$k.wav;done
 mkdir mono
-for k in *.wav; do ffmpeg -i $k -ac 1 mono/$k;done #converting files from multi channel to mono channel
+cd wav
+for k in *.wav; do ffmpeg -i $k -ac 1 ../mono/$k;done #converting files from multi channel to mono channel
+cd ..
 mkdir mono_16kz
 cd mono
 for k in *; do ffmpeg -i $k -ar 16000 ../mono_16kz/$k;done #converting n sample rate to 16KhZ
